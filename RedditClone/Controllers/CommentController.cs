4598da0e45e8776feecf0fc8462326f1bf09d415
brandon <brandonbrewer93@gmail.com>
2018-02-25 21:02:44 -0500

    Implement Add/Edit functionality to comments.

---
+++ RedditClone/Controllers/CommentController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Web;
+using System.Web.Mvc;
+using RedditClone.Models;
+
+namespace RedditClone.Controllers
+{
+    public class CommentController : Controller
+    {
+        // GET: Comment
+        [HttpPost]
+        public ActionResult AddComment(Comment newComment)
+        {
+            var postId = newComment.PostId;
+
+            using (var redditCloneContext = new RedditCloneContext())
+            {
+                var comment = new Comment
+                {
+                    CommentBody = newComment.CommentBody,
+                    PostId = newComment.PostId,
+                    Date = DateTime.Now
+                };
+
+                redditCloneContext.Comments.Add(comment);
+                redditCloneContext.SaveChanges();
+
+                return RedirectToAction("Index", "Post", new { id = postId });
+            }
+        }
+
+        [HttpPost]
+        public ActionResult EditComment(Comment currentComment)
+        {
+            using (var redditCloneContext = new RedditCloneContext())
+            {
+                var postId = currentComment.PostId;
+
+                var comment = redditCloneContext.Comments.SingleOrDefault(c => c.CommentId == currentComment.CommentId);
+
+                if( comment != null )
+                {
+                    comment.CommentBody = currentComment.CommentBody;
+                    redditCloneContext.SaveChanges();
+
+                    return RedirectToAction("Index", "Post", new {id = postId});
+                }
+            }
+
+            return new HttpNotFoundResult();
+        }
+    }
+}
